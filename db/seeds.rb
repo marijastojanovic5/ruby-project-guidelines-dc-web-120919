bb2233325ecefd802414a403181c55d62fc71788
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-12-30 11:59:28 -0500

    seeded data

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+ Reader.destroy_all
+ Book.destroy_all
+
+ 50. times do
+    Reader.create(name: Faker::Name.unique.name)
+  end
+  50. times do
+    Book.create(title: Faker::Book.title,author: Faker::Book.author)
+  end
+
